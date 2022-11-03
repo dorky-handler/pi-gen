77c1cf7ba498955bfa340069560786a255559a32
dorky-handler <48204960+dorky-handler@users.noreply.github.com>
2022-11-03 15:20:40 +0530

    Update 00-run-chroot.sh

--- stage3/01-tweaks/00-run-chroot.sh
+++ stage3/01-tweaks/00-run-chroot.sh
@@ -3,7 +3,7 @@
 
 cd /home/user
 #rm -r zcash
-#git clone https://github.com/dorky-handler/zcash.git
+git clone https://github.com/zcash/zcash.git
 cd zcash
 git checkout v5.3.0
 ./zcutil/fetch-params.sh
