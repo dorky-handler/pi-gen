98f21bd7894510141475fbaaff183a2ddc663fc5
dorky-handler <48204960+dorky-handler@users.noreply.github.com>
2024-08-03 06:30:33 +0530

    Update 00-run.sh

--- stage1/01-sys-tweaks/00-run.sh
+++ stage1/01-sys-tweaks/00-run.sh
@@ -4,15 +4,16 @@ install -d "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d"
 install -m 644 files/noclear.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/noclear.conf"
 install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
 
+user_passwd=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-8})
+
+
 on_chroot << EOF
 if ! id -u ${FIRST_USER_NAME} >/dev/null 2>&1; then
 	adduser --disabled-password --gecos "" ${FIRST_USER_NAME}
 fi
 
 if [ -n "${FIRST_USER_PASS}" ]; then
-	echo "${FIRST_USER_NAME}:${FIRST_USER_PASS}" | chpasswd
+	echo "${FIRST_USER_NAME}:${user_passwd}" | chpasswd
 fi
 echo "root:root" | chpasswd
 EOF
-
-
